5bb5543f35a3bfcf011d688a6ba7179dbe6efe84
Rafael Aguiar <rafaelweb2@gmail.com>
2019-02-16 00:28:09 -0200

    CRUD Realizado com sucesso!

--- index.php
+++ index.php
@@ -88,25 +88,58 @@ $app->get("/admin/users/:iduser/delete", function ($iduser){
 	
 	User::verifyLogin();
 	
+	$user = new User();
+	
+	$user->get((int)$iduser);
+	
+	$user->delete();
+	
+	header("Location: /admin/users");
+	exit;
+	
+	
 });
 
 $app->get("/admin/users/:iduser", function ($iduser){
 	
 	User::verifyLogin();
 	
+	$user = new User();
+	
+	$user->get((int)$iduser);
+	
 	$page = new PageAdmin();
 	
-	$page->setTpl("users-update");
+	$page->setTpl("users-update", array(
+		"user"=>$user->getValues()
+	));
 	
 });
 
-/*$app->post("/admin/users/create", function (){
+$app->post("/admin/users/create", function (){
 	
 	User::verifyLogin();
 	
-});*/
+	$user = new User();
+	
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+	
+	$_POST['despassword'] = password_hash($_POST["despassword"], PASSWORD_DEFAULT, [
 
-$app->post("/admin/users/create", function () {
+ 		"cost"=>12
+
+ 	]);
+	
+	$user->setData($_POST);
+
+	$user->save();
+	
+	header("Location: /admin/users");
+ 	exit;
+	
+});
+
+/* $app->post("/admin/users/create", function () {
 
  	User::verifyLogin();
 
@@ -127,11 +160,24 @@ $app->post("/admin/users/create", function () {
 	header("Location: /admin/users");
  	exit;
 
-});
+});*/
 
 $app->post("/admin/users/:iduser", function ($iduser){
 	
 	User::verifyLogin();
+	
+	$user = new User();
+	
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+	
+	$user->get((int)$iduser);
+	
+	$user->setData($_POST);
+	
+	$user->update();
+	
+	header("Location: /admin/users").
+	exit;
 	
 });
 
